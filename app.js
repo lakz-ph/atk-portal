04c13242eab42bed6afdc6b7fb18669692555b55
lakz-ph <lakshminarayanan093@gmail.com>
2026-02-20 02:26:07 +0530

    Added video background login page

--- app.js
+++
@@ -1,9 +0,0 @@
-window.toggleRC = function() {
-  const rc = document.getElementById("rcOptions");
-  rc.style.display = rc.style.display === "flex" ? "none" : "flex";
-};
-
-window.toggleACC = function() {
-  const acc = document.getElementById("accOptions");
-  acc.style.display = acc.style.display === "flex" ? "none" : "flex";
-};
